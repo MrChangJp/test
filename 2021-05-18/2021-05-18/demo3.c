982113ad14b683e5420f81ef00182f9975626a81
ChangJunpeng <1135130670@qq.com>
2021-05-18 20:18:35 +0800

    2021-05-18复习代码

---
+++ 2021-05-18/2021-05-18/demo3.c
@@ -0,0 +1,46 @@
+//#define _CRT_SECURE_NO_WARNINGS 10
+//
+//
+//
+//#if 1==1
+//;
+//#elif 2==2
+//;
+//else
+//;
+//#endif
+//#pragma pack
+//#pragma once
+//
+//#ifndef STDIO_H
+//#define STDIO_H
+//
+//#include <stdio.h>
+//
+//#endif
+//
+//#include<stdio.h>
+//
+//#define countHong(type,val_name) (int)&(((type*)0)->val_name)
+//#define printfs(x,y) printf("put "#x" value is %d",y);
+//
+//struct ss{
+//	int a;
+//	char b;
+//	double c;
+//};
+//enum testEnum{
+//	test,
+//	tes2
+//};
+//
+//int main(){
+//
+//	printfs(a,10);
+//
+//	printf("%d\n", countHong(struct ss,a));
+//	printf("%d\n", countHong(struct ss,b));
+//	printf("%d\n", countHong(struct ss, c));
+//
+//	return 0;
+//}
